f519f230e23e91714832d8f53e6cdecbbcd81168
Dilshad <helloshad3@gmail.com>
2025-12-07 12:12:02 +0530

    RBAC Menu for Mobile

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -4,8 +4,10 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import { sidebarItems } from "./SidebarItems";
+import { useMe } from "@/hooks/useAuth";
 
 export default function MobileSidebar() {
+  const { data: user } = useMe();
   return (
     <Sheet>
       <SheetTrigger className="md:hidden">
@@ -14,16 +16,18 @@ export default function MobileSidebar() {
 
       <SheetContent side="left" className="p-0 w-64">
         <nav className="mt-6 px-4">
-          {sidebarItems.map((item) => (
-            <Link
-              href={item.href}
-              key={item.href}
-              className="flex items-center gap-3 p-2 rounded-lg hover:bg-neutral-100 dark:hover:bg-neutral-800"
-            >
-              <item.icon className="h-5 w-5" />
-              <span>{item.name}</span>
-            </Link>
-          ))}
+          {sidebarItems
+            .filter((item) => item.roles.includes(user?.role))
+            .map((item) => (
+              <Link
+                href={item.href}
+                key={item.href}
+                className="flex items-center gap-3 p-2 rounded-lg hover:bg-neutral-100 dark:hover:bg-neutral-800"
+              >
+                <item.icon className="h-5 w-5" />
+                <span>{item.name}</span>
+              </Link>
+            ))}
         </nav>
       </SheetContent>
     </Sheet>
